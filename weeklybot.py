69ad9f6bfffc8b792290046192fabd60ebaa9eca
Arstin Reano <arstingabrelreano@gmail.com>
2024-05-12 23:10:26 +0800

    Final draft

--- weeklybot.py
+++ weeklybot.py
@@ -1,44 +1,44 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext, JobQueue
+from telegram.ext import Updater, CommandHandler, CallbackContext
 import datetime
+import pytz
+
+TOKEN = '7083104022:AAG7Q2dWOPyq5N1Do3IhaMeq7zGWiHycaRo'
+MESSAGE = "ACCOUNTABILITY CHECK\n\nWho did you reach out to last week? Any response?\n\nWho will you be reaching out to this week?"
 
 def start(update: Update, context: CallbackContext):
     """Send a message when the command /start is issued."""
     update.message.reply_text('Hello! I am your bot, ready to send reminders.')
     context.chat_data['chat_id'] = update.message.chat_id
 
+def send_now(update: Update, context: CallbackContext):
+    """Send the message immediately when /go is used."""
+    chat_id = update.message.chat_id
+    context.bot.send_message(chat_id=chat_id, text=MESSAGE)
+
 def send_weekly_message(context: CallbackContext):
     """Function to send the weekly message."""
-    job = context.job
-    context.bot.send_message(chat_id=job.context, text="Hello, this is your weekly reminder!")
+    chat_id = context.job.context
+    context.bot.send_message(chat_id=chat_id, text=MESSAGE)
 
-def send_now(update: Update, context: CallbackContext):
-    chat_id = update.message.chat_id
-    context.bot.send_message(chat_id=chat_id, text="Sending this now to make sure it works")
-
-def schedule_message(job_queue):
+def schedule_message(job_queue, chat_id):
     """Schedule the weekly message using JobQueue."""
-    # Target time: Monday at 9 AM
-    time = datetime.time(hour=9, minute=0, second=0)
-    # context argument is used to pass the chat_id
-    job_queue.run_daily(send_weekly_message, time, days=(0,), context='-1002076218797')  # Example chat_id
+    phtime = pytz.timezone('Asia/Manila')
+    # Target time: Monday at 9 AM PHT
+    time = datetime.time(hour=23, minute=7, second=0, tzinfo=phtime)
+    job_queue.run_daily(send_weekly_message, time, days=(6,), context=chat_id)
 
 def main():
-    """Main function to start the bot."""
-    TOKEN = '7083104022:AAG7Q2dWOPyq5N1Do3IhaMeq7zGWiHycaRo'
-    updater = Updater(TOKEN, use_context=True)
+    updater = Updater(token=TOKEN, use_context=True)
 
-    # Register handlers
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("go", send_now))
 
-
-    # Schedule messages
+    # Example chat ID, replace with actual chat ID or fetch dynamically
     chat_id = '-1002076218797'
-    schedule_message(updater.job_queue)
+    schedule_message(updater.job_queue, chat_id)
 
-    # Start the bot
     updater.start_polling()
     updater.idle()
 
